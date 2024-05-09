7045e73c3ead6f6e6761cfc770201c60edd66ded
ary4nsinha <115373151+ary4nsinha@users.noreply.github.com>
2024-05-09 21:59:37 +0530

    update

--- textExtraction.py
+++ textExtraction.py
@@ -4,7 +4,7 @@ import cv2
 import matplotlib.pyplot as plt
 
 myconfig = r"--psm 6 --oem 3"
-img_path = "assets/text3.jpg"
+img_path = "assets/numberPlate3.jpg"
 img = plt.imread(img_path)
 text = pytesseract.image_to_string(PIL.Image.open(img_path), config=myconfig)
 print(text)
